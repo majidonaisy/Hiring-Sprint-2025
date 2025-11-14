9eec5ee163110f7a1cbde01d27cc6fb1b9c76e28
majidonaisy <onaisym4@gmail.com>
2025-11-14 09:56:46 +0200

    setup project phase 2 : INITIAL MODEL ANALYSIS + IMAGE STORAGE + OPENAPI DOCUMENTATION

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import HomePage from './pages/HomePage';
 // import InspectionPage from './pages/InspectionPage';
 // import ReportPage from './pages/ReportPage';
